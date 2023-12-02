dbc3452b403d6b223cc48d4261e07c2d5b81d480
Antoine Debroye <ant.debroye@gmail.com>
2023-12-02 21:23:43 +0100

    antoine/python: solve the 2nd challenge of day 1
    Add a new function to transform digit in letters to real
    digits.

--- antoine/python/day1/day1.py
+++ antoine/python/day1/day1.py
@@ -1,14 +1,31 @@
-def extract(file):  
-    with open(file, 'r') as file_name:
-        line_res = []
-        for line in file_name:
-            numbers = [char for char in line if char.isdigit()]
-            line_res.append(int(numbers[0]) * 10 + int(numbers[len(numbers)-1]))
-        return line_res
+numbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
 
-extract_total = extract("day1.txt")
-res = 0
-for value in extract_total:
-    res += value
+def words_to_line(file):
+    new_file = []
+    for line in file:
+        for i, number in enumerate(numbers):
+            if number in line:
+                # Not the best way to do it for sure.
+                # Here I let the first and last letter of the digit to avoid removing one digit
+                # like with eightwo wich is 82 and not only 8
+                line = line.replace(number, number[0]+str(i+1)+number[len(number)-1])
+        new_file.append(line)
+    return new_file
 
-print(res)
+
+def extract(file):
+    line_res = []
+    for line in file:
+        numbers = [char for char in line if char.isdigit()]
+        line_res.append(int(numbers[0]) * 10 + int(numbers[len(numbers)-1]))
+    return line_res
+
+
+with open("day1.txt", 'r') as filename:
+    file = words_to_line(filename)
+    extract_total = extract(file)
+    res = 0
+    for value in extract_total:
+        res += value
+    
+    print(res)
