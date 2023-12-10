cce8900a929d82bfe8db7cde2624a1d2c8591d29
Antoine Debroye <ant.debroye@gmail.com>
2023-12-10 11:08:56 +0100

    antoine/python: Solve 2nd part of day 2
    refact the parse file function to find all Max by color

--- antoine/python/day2/day2.py
+++ antoine/python/day2/day2.py
@@ -2,7 +2,7 @@ import re
 
 rule = {'red': 12, 'blue': 14, 'green': 13}
 
-def parse_file(file):
+def parse_file(file, Max=False):
     result = []
     for line in file:
         count_red = 0
@@ -13,16 +13,32 @@ def parse_file(file):
             for color in rule:
                 match = re.search(r'\b(\d+)\s' + re.escape(color), substring)
                 if match:
+                    count = int(match.group(1))
                     if color == 'red':
-                        count_red = int(match.group(1))
+                        if Max:
+                            if count > count_red:
+                                count_red = count
+                        else:
+                            count_red = count
+
                     elif color == 'blue':
-                        count_blue = int(match.group(1))
+                        if Max:
+                            if count > count_blue:
+                                count_blue = count
+                        else:
+                            count_blue = count
                     else:
-                        count_green = int(match.group(1))
-            if count_red > rule['red'] or count_blue > rule['blue'] or count_green > rule['green']:
+                        if Max:
+                            if count > count_green:
+                                count_green = count
+                        else:
+                            count_green = count
+            if (count_red > rule['red'] or count_blue > rule['blue'] or count_green > rule['green']) and not Max:
                 break
-        if count_red <= rule['red'] and count_blue <= rule['blue'] and count_green <= rule['green']:
+        if (count_red <= rule['red'] and count_blue <= rule['blue'] and count_green <= rule['green']) and not Max:
             result.append(line)
+        if Max:
+            result.append([count_red, count_blue, count_green])
     return result
 
 
@@ -31,12 +47,25 @@ def sum_games(games):
     for game in games:
         match = re.match(r'Game (\d+)', game)
         if match:
-            print(match.group(1))
             sum += int(match.group(1))
     print(sum)
 
 
+def find_max(file):
+    result = 0
+    max_lines = parse_file(file, Max=True)
+    for line in max_lines:
+        intermediate = 1
+        for part in line:
+            intermediate = intermediate * part
+        result += intermediate
+    print(result)
+
+
 
 with open("day2.txt", 'r') as filename:
-    parsed_file = parse_file(filename)
+    tmp1 = list(filename)
+    tmp2 = tmp1[:]
+    parsed_file = parse_file(tmp2)
     sum_games(parsed_file)
+    find_max(tmp1)
